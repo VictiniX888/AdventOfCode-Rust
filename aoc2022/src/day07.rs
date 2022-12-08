d67e8aeb52955535ce64013f36bbd6a6959247fe
Jiong Yan Yap <jiongyan@usc.edu>
2022-12-08 11:39:30 -0800

    Optimize 2022 Day 7

--- aoc2022/src/day07.rs
+++ aoc2022/src/day07.rs
@@ -1,22 +1,35 @@
 use std::{
     cell::RefCell,
-    collections::BTreeSet,
+    collections::{BTreeMap, HashMap},
     rc::{Rc, Weak},
 };
 
 use crate::*;
 
-pub const SOLUTION: Solution = Solution { day: 7, solve };
+pub const SOLUTION: Solution = Solution {
+    day: 7,
+    solve: solve_map,
+};
 
-// 510 us
-fn solve(input: &str) -> AnswerSet {
-    let filesystem = build_filesystem(input);
+/* ======== OPTIMIZED HASHMAP ======== */
+// 225 us
+fn solve_map(input: &str) -> AnswerSet {
+    let filesystem = build_filesystem_map(input);
 
-    let mut ordered_sizes = BTreeSet::new();
-    let (_, p1) = sum_sizes(&filesystem, 100000, &mut ordered_sizes);
+    let mut p1 = 0;
+    let mut p2 = u64::MAX;
 
-    let min_delete = ordered_sizes.iter().next_back().unwrap() - 40000000;
-    let &p2 = ordered_sizes.range(min_delete..).next().unwrap();
+    let total_size = filesystem.get("/").unwrap();
+
+    for size in filesystem.values() {
+        if size <= &100000 {
+            p1 += size;
+        }
+
+        if size >= &(total_size - 40000000) && size < &p2 {
+            p2 = *size;
+        }
+    }
 
     AnswerSet {
         p1: Answer::U64(p1),
@@ -24,30 +37,112 @@ fn solve(input: &str) -> AnswerSet {
     }
 }
 
-fn sum_sizes(folder: &Folder, limit: u64, sizes: &mut BTreeSet<u64>) -> (u64, u64) {
+fn build_filesystem_map(input: &str) -> HashMap<String, u64> {
+    let mut stack = Vec::new();
+    let mut filepath = "/".to_string();
+    stack.push(filepath.to_string());
+
+    let mut filesystem = HashMap::new();
+    filesystem.insert(filepath.to_string(), 0);
+
+    for line in input.lines().skip(1) {
+        let mut bytes = line.bytes();
+
+        let front = bytes.next().unwrap();
+        match front {
+            b'$' => {
+                let command = bytes.nth(1).unwrap();
+                match command {
+                    b'c' => {
+                        // $ cd
+                        // Consume d and space
+                        bytes.nth(1);
+
+                        // Get folder name
+                        // This also assumes that every ls command will be immediately preceded by a cd command which explicitly states the folder name
+                        let curr_folder_name: String = bytes.map(|b| b as char).collect();
+
+                        if curr_folder_name == ".." {
+                            let last_folder = stack.pop().unwrap();
+                            let &last_size = filesystem.get(&filepath).unwrap_or(&0);
+                            filepath.truncate(filepath.len() - last_folder.len() - 1);
+                            *filesystem.get_mut(&filepath).unwrap() += last_size;
+                        } else {
+                            filepath.push('/');
+                            filepath.push_str(&curr_folder_name);
+                            stack.push(curr_folder_name);
+
+                            // This assumes that no folder is visited from the top-down more than once
+                            filesystem.insert(filepath.clone(), 0);
+                        }
+                    }
+
+                    _ => {
+                        // $ ls
+                        // Do nothing
+                    }
+                }
+            }
+
+            b'd' => {
+                // dir
+            }
+
+            _ => {
+                let size = parse_u64_from_ascii_iter(&mut bytes, front);
+
+                *filesystem.get_mut(&filepath).unwrap() += size;
+            }
+        }
+    }
+
+    // Unwind stack
+    while stack.len() > 1 {
+        let last_folder = stack.pop().unwrap();
+        let &last_size = filesystem.get(&filepath).unwrap_or(&0);
+        filepath.truncate(filepath.len() - last_folder.len() - 1);
+        *filesystem.get_mut(&filepath).unwrap() += last_size;
+    }
+
+    filesystem
+}
+
+/* ======== LINKED TREE SOLUTION ======== */
+// ~500 us
+fn solve(input: &str) -> AnswerSet {
+    let filesystem = build_filesystem(input);
+
+    let mut ordered_sizes = BTreeMap::new();
+    let root_size = sum_sizes(&filesystem, &mut ordered_sizes);
+
+    let p1 = ordered_sizes
+        .range(..=100000)
+        .map(|(size, &freq)| size * freq as u64)
+        .sum();
+
+    let min_delete = root_size - 40000000;
+    let &p2 = ordered_sizes.range(min_delete..).next().unwrap().0;
+
+    AnswerSet {
+        p1: Answer::U64(p1),
+        p2: Answer::U64(p2),
+    }
+}
+
+fn sum_sizes(folder: &Folder, sizes: &mut BTreeMap<u64, usize>) -> u64 {
     let size = folder.files.borrow().iter().map(|file| file.size).sum();
     let folders = folder.folders.borrow();
 
     if folders.is_empty() {
-        sizes.insert(size);
-        (size, if size <= limit { size } else { 0 })
+        // Need to do this to keep track of duplicate sizes
+        *sizes.entry(size).or_insert(0) += 1;
+        size
     } else {
-        let (sub_total, sub_under) = folders
-            .iter()
-            .map(|folder| sum_sizes(folder, limit, sizes))
-            .reduce(|(acc_total, acc_under), (total, under)| (acc_total + total, acc_under + under))
-            .unwrap();
+        let sub_total: u64 = folders.iter().map(|folder| sum_sizes(folder, sizes)).sum();
 
         let size = sub_total + size;
-        sizes.insert(size);
-        (
-            size,
-            if size <= limit {
-                size + sub_under
-            } else {
-                sub_under
-            },
-        )
+        *sizes.entry(size).or_insert(0) += 1;
+        size
     }
 }
 
@@ -97,12 +192,10 @@ fn build_filesystem(input: &str) -> Rc<Folder> {
                         }
                     }
 
-                    b'l' => {
+                    _ => {
                         // $ ls
                         // Do nothing
                     }
-
-                    _ => {}
                 }
             }
 
